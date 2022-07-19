de0ae542027ae6ec07792c83568d02504f188bcb
Julien <julienmulle@gmail.com>
2022-07-19 08:00:30 +0200

    ajout de la methode attack

--- Monster.java
+++ Monster.java
@@ -1,14 +1,31 @@
+// voir diapo dans drive poeiBordeaux/s8/java3 programmation orientée objet
 public class Monster {
     private String name;
     private int damage;
     private int life;
 
+    //constructor
     public Monster(String cName, int cDamage, int cLife){
         this.name = cName;
         this.damage = cDamage;
         this.life = cLife;
     }
 
+    //methode
+    public void attack(Monster enemy){
+        //je veux acceder à la vie de l'ennemy et modifier sa vie
+        enemy.setLife(enemy.getLife() - this.getDamage());
+
+        // si l'enemy à + de 0 de vie, affiche moi le nom de l'ennemy + le reste de sa vie
+        if(enemy.getLife()>0){
+            System.out.println(enemy.getName() + "has" + enemy.getLife()+" points remaining");
+        }
+        // sinon affiche moi le message 
+        else {
+            System.out.println(enemy.getName()+ " is KO !");
+        }
+    }
+    //guetteur-setteur
     public String getName() {
         return this.name;
     }
@@ -34,5 +51,7 @@ public class Monster {
     public String toString(){
         return "Monter [damage=" + damage + ", life=" + life + ", name=" + name +"]";
     }
+
+
 
 }
