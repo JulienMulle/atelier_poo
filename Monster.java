b17c54068b2bc74f833ec35c38ac761104326931
Julien <julienmulle@gmail.com>
2022-07-19 07:34:40 +0200

    first commit

---
+++ Monster.java
@@ -0,0 +1,27 @@
+public class Monster {
+    private String name;
+    private int damage;
+    private int life;
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getLife() {
+        return this.life;
+    }
+    public void setLife(int life) {
+        this.life= life;
+    }
+
+}
