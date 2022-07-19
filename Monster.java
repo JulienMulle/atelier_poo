47c6184d49c91a34c22506866b04fec443610ad0
Julien <julienmulle@gmail.com>
2022-07-19 07:40:22 +0200

    creation class Monster

--- Monster.java
+++ Monster.java
@@ -3,6 +3,12 @@ public class Monster {
     private int damage;
     private int life;
 
+    public Monster(String cName, int cDamage, int cLife){
+        this.name = cName;
+        this.damage = cDamage;
+        this.life = cLife;
+    }
+
     public String getName() {
         return this.name;
     }
@@ -22,6 +28,11 @@ public class Monster {
     }
     public void setLife(int life) {
         this.life= life;
+    }
+
+    @Override
+    public String toString(){
+        return "Monter [damage=" + damage + ", life=" + life + ", name=" + name +"]";
     }
 
 }
